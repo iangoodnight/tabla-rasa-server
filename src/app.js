5de33010a60828bed4341d3beaaab3ea7057d2d8
Ian Goodnight <goodnight.ian@gmail.com>
2021-05-13 16:56:34 -0400

    added route handling for monorepo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@
 'use strict';
 
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -17,7 +16,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routing
 const routes = require('./routes');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 const apiRoutes = require('./api');
-const viewRoutes = require('./view');
+const { log } = console;
+const chalk = require('chalk');
 
+// serve up api routes from 'api' directory
 router.use('/api', apiRoutes);
-router.use('/', viewRoutes);
+
+// test for monorepo
+try {
+  if (fs.existsSync(path.join(__dirname, '../../../client'))) {
+    log(chalk.green('Monorepo detected... serving client'));
+    // serve up static assets
+    router.use(express.static(path.join(__dirname, '../../../client/build')));
+    router.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../../../client/build/index.html'));
+    });
+  }
+} catch (e) {
+  log(chalk.red('No client detected, loading API only'));
+}
 
 module.exports = router;
